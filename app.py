a039c0c8428295aac68128d56e31149fd2bf119b
VINITHA.V <vvinithavviitha698@gmail.com>
2026-01-22 19:32:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "HEAD"])
 def home():
     return render_template("index.html")
+
 
 @app.route("/chat", methods=["POST"])
 def chat():
@@ -21,3 +22,4 @@ def chat():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
