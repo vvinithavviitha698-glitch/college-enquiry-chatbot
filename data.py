8f001659c1fbde479af7e7fa959bb00fe5eb4623
VINITHA.V <vvinithavviitha698@gmail.com>
2026-01-22 21:38:57 +0530

    Refactor college_info dictionary structure

--- data.py
+++ data.py
@@ -1,13 +1,13 @@
 college_info = {
-    "courses":
+    "ug or pg":
     "We offer B.Sc, B.Com, BA, Costume design and Msc.",
-    "BSc courses":
+    "Bsc":
     "BSc Computer Science, BSc Mathematics, BSc Chemistry, BSc Costume design & Fasion.",
-    "B.Com courses":
+    "B.Com":
     "B.Com, B.Com Computer Application, B.Com Banking & Insurance.",
-    "BA courses":
+    "BA":
     "BA Tamil, BA English.",
-    "MSc courses":
+    "MSc":
     "MSc Computer Science.",
     "admission":
     "Admissions are based on councelling and online application.",
@@ -50,3 +50,4 @@ college_info = {
     "canteen":
     "The college canteen provides hygienic and affordable snacks, drinks,& tea/coffee,milk for students and staff."
 }
+
